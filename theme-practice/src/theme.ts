09a031fb94d0f743e374a297c9eb09a12a77542c
jiyoonZ <wldbsl416@gmail.com>
2022-09-10 17:26:55 +0900

    Feat : ThemeProvider 작성

--- theme-practice/src/theme.ts
+++ theme-practice/src/theme.ts
@@ -1,12 +1,14 @@
 import {DefaultTheme} from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
-};
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  btnColor: "teal",
-};
+// export const theme: DefaultTheme = {
+//   // dark: {
+//   //   bgColor: "#2f3640",
+//   //   textColor: "#f5f6fa",
+//   //   accentColor: "#8c7ae6",
+//   // },
+//   // light: {
+//   //   bgColor: "#f5f6fa",
+//   //   textColor: "#2f3640",
+//   //   accentColor: "#8c7ae6",
+//   // },
+// };
