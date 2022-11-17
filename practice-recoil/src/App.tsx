2f7ed205b93fb5ba92c623c3c8280a636555b5bc
jiyoonZ <wldbsl416@gmail.com>
2022-11-17 13:52:04 +0900

    selector set property

--- practice-recoil/src/App.tsx
+++ practice-recoil/src/App.tsx
@@ -1,9 +1,33 @@
+import React from "react";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {hourSelector, minuteState} from "./atoms";
+
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onMinutesChange = (evt: React.FormEvent<HTMLInputElement>) => {
+    console.log(evt.currentTarget.value);
+    setMinutes(+evt.currentTarget.value);
+  };
+  const onHoursChange = (evt: React.FormEvent<HTMLInputElement>) => {
+    setHours(+evt.currentTarget.value);
+  };
   return (
     <>
       <h1>🚀 BOOM!</h1>
-      <input type="number" placeholder="Minutes" />
-      <input type="number" placeholder="Hours" />
+      <input
+        value={minutes}
+        type="number"
+        placeholder="Minutes"
+        onChange={onMinutesChange}
+      />
+      <input
+        value={hours}
+        type="number"
+        placeholder="Hours"
+        onChange={onHoursChange}
+      />
     </>
   );
 }
