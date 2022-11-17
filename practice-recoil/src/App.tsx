3e27e513f13bbbc467088c158694820e34da19dd
jiyoonZ <wldbsl416@gmail.com>
2022-11-17 11:39:58 +0900

    setting for recoil

---
+++ practice-recoil/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>🚀 BOOM!</h1>
+      <input type="number" placeholder="Minutes" />
+      <input type="number" placeholder="Hours" />
+    </>
+  );
+}
+
+export default App;
---
+++ practice-recoil/src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {RecoilRoot} from "recoil";
+import {ThemeProvider} from "styled-components";
+import App from "./App";
+import GlobalStyle from "./GlobalStyle";
+import {darktheme} from "./theme";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={darktheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
